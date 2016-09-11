81b7c8784f2062d01c5735db55293c44bfc95e05
Matthew Soh <matthewsoh.cw@gmail.com>
2016-09-11 01:32:56 -0700

    Uber

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -1,5 +1,22 @@
-class EstimatesController < ApplicationController
+class EstimatesController < ApplicationController 
   def show
-    render :json => params
+    render :json => uber_time(params[:start_lat], params[:start_lng], params[:end_lat], params[:end_lng])
+  end
+
+  def uber(start_lat, start_lng, end_lat, end_lng)
+    uber_base_url = 'https://api.uber.com/v1/'
+    query = {
+      'start_latitude' => start_lat,
+      'start_longitude' => start_lng,
+      'end_latitude' => end_lat,
+      'end_longitude' => end_lng
+    }
+
+    headers = { 'Authorization' => 'Token ' + $UBER_SERVER_TOKEN }
+    response = HTTParty.get(uber_base_url + 'estimates/price/', :query => query, :headers => headers)
+    return response 
+  end
+  
+  def lyft(start_lat, start_lng, end_lat, end_lng)
   end
 end

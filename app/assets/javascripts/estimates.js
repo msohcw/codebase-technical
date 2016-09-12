a361e785afb3bf593855141b7fd02cff2e275452
Matthew Soh <matthewsoh.cw@gmail.com>
2016-09-12 11:14:43 -0700

    Added JS to run form, imported Google Maps API

--- app/assets/javascripts/estimates.js
+++ app/assets/javascripts/estimates.js
@@ -1,4 +1,58 @@
-console.log("Loaded")
+console.log("Loaded");
+
+var _Geocoder;
+
 $(document).ready(function(){
-  console.log("jQuery Loaded")
+  console.log("jQuery loaded");
+  
+  $('#est-submit').click(function(){
+    start = $('#est-start-input').val();
+    end = $('#est-end-input').val();
+    startLatLng = latLngPair(start);
+    endLatLng = latLngPair(end);
+    
+    if(!startLatLng) startLatLng = geocode(start);
+    if(!endLatLng) endLatLng = geocode(end);
+    compare(startLatLng, endLatLng);
+  })
+  
 })
+
+function initGoogleMaps(){
+  console.log("Google Maps API loaded");
+  _Geocoder = new google.maps.Geocoder();
+}
+
+function latLngPair(string){
+  var valid = new RegExp(/-?[0-9]+\.[0-9]+[^0-9-]+-?[0-9]+\.[0-9]+/, 'g');
+  var each = new RegExp(/-?[0-9]+\.[0-9]+((?=[^0-9-])|(?=$))/, 'g');
+
+  if(!valid.test(string)) return false;
+
+  var matches = [];
+  string.replace(each, function(x){
+    matches.array.push(x);
+    return x;
+  })
+  
+  var latLng = {'lat': matches[0], 'lng': matches[1]};
+  return latLng;
+}
+
+function geocode(location){
+  _Geocoder.geocode({'address':location}, function(results, status){
+    if(status == 'OK'){
+      var latLng = results[0].geometry.location;
+      return {'lat' : latLng.lat(), 'lng' : latLng.lng()};
+    }else{
+      console.log('Geocoding failed. Tried to geocode ' + location +'. Got ' + status);
+      return false;
+    }
+  }
+  })
+}
+
+function compare(startLatLng, endLatLng){
+  console.log(startLatLng);
+  console.log(endLatLng);
+}
